b6c6551622d3c55c71043b8bda14a1c7ed0a2ccd
Nitin308 <nitin3081998@gmail.com>
2022-12-05 20:10:49 +0530

    Find the Max Integer

---
+++ TC1.1_MaxNumberPosition1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Based Problems");
+
+            int output = MaxNumberCheck.MaxIntegerNumber(1111, 111, 11);
+            Console.WriteLine("\nMax integer number is: " + output);
+        }
+    }
+}
