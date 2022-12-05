2c496ca22a8ebb5837b5711570d9ecc9d3665024
Nitin308 <nitin3081998@gmail.com>
2022-12-05 21:09:35 +0530

    Max Float at 3rd Position

---
+++ TC2.3_MaxFloatPosition3/MaxNumberCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblems
+{
+    public class MaxNumberCheck
+    {
+        public static float MaxIntegerNumber(float val1, float val2, float val3)
+        {
+            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0)
+                return val1;
+            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0)
+                return val2;
+            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0)
+                return val3;
+            return val3;
+        }
+    }
+}
