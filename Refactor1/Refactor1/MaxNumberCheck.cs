1b9547a84483d9f55599f906be97a6390b58946b
Nitin308 <nitin3081998@gmail.com>
2022-12-05 18:02:38 +0530

    Refactor using Generic Method

---
+++ Refactor1/Refactor1/MaxNumberCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblems
+{
+    public class MaxNumberCheck
+    {
+        public T MaxNumber<T>(T val1, T val2, T val3) where T : IComparable
+        {
+            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0)
+                return val1;
+            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0)
+                return val2;
+            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0)
+                return val3;
+            return val1;
+        }
+    }
+}
---
+++ Refactor1/Refactor1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Based Problems");
+
+            MaxNumberCheck objMaxNumber = new MaxNumberCheck();
+
+            int output1 = objMaxNumber.MaxNumber<int>(11, 111, 1111);
+            Console.WriteLine("\nMax integer number is: " + output1);
+
+            float output2 = objMaxNumber.MaxNumber<float>(1, 11, 111);
+            Console.WriteLine("\nMax float number is: " + output2);
+
+            string output3 = objMaxNumber.MaxNumber<string>("Apple", "Peach", "Banana");
+            Console.WriteLine("\nMax string word is: " + output3);
+        }
+    }
+}
