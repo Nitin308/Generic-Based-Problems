f8b8eec9d1d0c64add570dc1ddd00a1f0d37a748
Nitin308 <nitin3081998@gmail.com>
2022-12-05 21:21:50 +0530

    Find Max String Word

---
+++ TC3.1_MaxStringPosition1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Based Problems");
+
+            string output = MaxNumberCheck.MaxIntegerNumber("Peach", "Apple", "Banana");
+            Console.WriteLine("\nMax string word is: " + output);
+        }
+    }
+}
