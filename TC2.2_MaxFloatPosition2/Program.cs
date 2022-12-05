144508ab9cb595761a8274a6bc7bc4441edd2def
Nitin308 <nitin3081998@gmail.com>
2022-12-05 20:55:48 +0530

    Max Float at 2nd Position

---
+++ TC2.2_MaxFloatPosition2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Based Problems");
+
+            float output = MaxNumberCheck.MaxIntegerNumber(1, 111, 11);
+            Console.WriteLine("\nMax float number is: " + output);
+        }
+    }
+}
