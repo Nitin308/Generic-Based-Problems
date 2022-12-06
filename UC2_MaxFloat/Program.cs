d6ac3c8973df9f0935740da42e09d1824d49c148
Nitin308 <nitin3081998@gmail.com>
2022-12-06 11:05:07 +0530

    Find Max Float Number

---
+++ UC2_MaxFloat/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Based Problems");
+
+            float output = MaxNumberCheck.MaxIntegerNumber(11, 111, 1111);
+            Console.WriteLine("\nMax float number is: " + output);
+        }
+    }
+}
