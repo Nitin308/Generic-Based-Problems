c342118a73d9b089b10a29f40602966b67e0908f
Nitin308 <nitin3081998@gmail.com>
2022-12-06 11:18:26 +0530

    Find Max String Word

---
+++ UC3_MaxString/MaxNumberCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblems
+{
+    public class MaxNumberCheck
+    {
+        public static string MaxIntegerNumber(string val1, string val2, string val3)
+        {
+            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0)
+                return val1;
+            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0)
+                return val2;
+            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0)
+                return val3;
+            return val1;
+        }
+    }
+}
