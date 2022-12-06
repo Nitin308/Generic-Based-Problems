c4e03e3b18084b5d109f9ec0734264b59aa8eac2
Nitin308 <nitin3081998@gmail.com>
2022-12-06 13:35:11 +0530

    Find max value

---
+++ MaxValue/Find max value/MaxNumberCheck.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Problems");
+
+            MaxNumberCheck<int> objint = new MaxNumberCheck<int>(11, 111, 1111, 11111);
+            Console.WriteLine("\nMax integer number is: " + objint.MaxMethod());
+
+            MaxNumberCheck<float> objfloat = new MaxNumberCheck<float>(1, 11, 111, 1111);
+            Console.WriteLine("\nMax float number is: " + objfloat.MaxMethod());
+
+            MaxNumberCheck<string> objstring = new MaxNumberCheck<string>("Apple", "Peach", "Banana", "Orange");
+            Console.WriteLine("\nMax string word is: " + objstring.MaxMethod());
+
+            int[] arr = { 11, 111, 1111, 11111, 111111 };
+            MaxNumberCheck<int> objvalue = new MaxNumberCheck<int>(arr);
+            objvalue.PrintMaxValue();
+        }
+    }
+}
